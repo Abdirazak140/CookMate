36685ea6a050aea01b07698c43219ffdbc80da61
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-05-11 00:42:06 -0400

    Adds buttons and logic for login

--- src/main/java/com/example/cookmate/StartUpPageController.java
+++ src/main/java/com/example/cookmate/StartUpPageController.java
@@ -2,13 +2,75 @@ package com.example.cookmate;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;import javafx.util.Pair;
+
 
 public class StartUpPageController {
     @FXML
-    private Label welcomeText;
+    private Label statusText;
 
     @FXML
     protected void onLoginButtonClick() {
-        welcomeText.setText("Signing Up for Cookmate!");
+        statusText.setText("Logging in to Cookmate!");
+
+        // new dialog for login
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Cookmate Login");
+        dialog.setHeaderText("Enter your username and password");
+
+        // Sets button
+        ButtonType loginButtonType = new ButtonType("Login", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+
+        // makes username and password labels/fields
+        TextField username = new TextField();
+        username.setPromptText("Username");
+        PasswordField password = new PasswordField();
+        password.setPromptText("Password");
+
+        grid.add(new Label("Username:"), 0, 0);
+        grid.add(username, 1, 0);
+        grid.add(new Label("Password:"), 0, 1);
+        grid.add(password, 1, 1);
+
+        dialog.getDialogPane().setContent(grid);
+
+        // request focus on username by default
+        javafx.application.Platform.runLater(username::requestFocus);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == loginButtonType) {
+                return new Pair<>(username.getText(), password.getText());
+            }
+            return null;
+        });
+
+        dialog.showAndWait();
+    }
+
+    @FXML
+    protected void onSignupButtonClick() {
+        statusText.setText("Signing Up for Cookmate!");
+        // TODO: add login for button
+    }
+
+    @FXML
+    protected void onGoogleLoginButtonClick() {
+        statusText.setText("Logging in with Google!");
+        // TODO: add login for button
+    }
+
+    @FXML
+    protected void onFacebookLoginButtonClick() {
+        statusText.setText("Logging in with Facebook!");
+        // TODO: add login for button
     }
 }
